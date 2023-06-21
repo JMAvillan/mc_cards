31ef22c4564487cab89857f59f6dfeae0ba203aa
JMAvillan <josemavillan45@gmail.com>
2023-06-21 18:53:55 -0400

    Updated React Native version + Android React Native config changes

--- android/app/src/main/java/com/mc/companion/MainActivity.java
+++ android/app/src/main/java/com/mc/companion/MainActivity.java
@@ -1,18 +1,19 @@
-package com.avillan.mc_cards;
+package com.mc.companion;
 
 import android.os.Build;
 import android.os.Bundle;
 
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
-import com.facebook.react.ReactRootView;
+import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
+import com.facebook.react.defaults.DefaultReactActivityDelegate;
 
 import expo.modules.ReactActivityDelegateWrapper;
 
 public class MainActivity extends ReactActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
-    // Set the theme to AppTheme BEFORE onCreate to support 
+    // Set the theme to AppTheme BEFORE onCreate to support
     // coloring the background, status bar, and navigation bar.
     // This is required for expo-splash-screen.
     setTheme(R.style.AppTheme);
@@ -28,17 +29,32 @@ public class MainActivity extends ReactActivity {
     return "main";
   }
 
+  /**
+   * Returns the instance of the {@link ReactActivityDelegate}. Here we use a util
+   * class {@link
+   * DefaultReactActivityDelegate} which allows you to easily enable Fabric and
+   * Concurrent React
+   * (aka React 18) with two boolean flags.
+   */
   @Override
   protected ReactActivityDelegate createReactActivityDelegate() {
-    return new ReactActivityDelegateWrapper(this,
-      new ReactActivityDelegate(this, getMainComponentName())
+    return new DefaultReactActivityDelegate(
+        this,
+        getMainComponentName(),
+        // If you opted-in for the New Architecture, we enable the Fabric Renderer.
+        DefaultNewArchitectureEntryPoint.getFabricEnabled(), // fabricEnabled
+        // If you opted-in for the New Architecture, we enable Concurrent React (i.e.
+        // React 18).
+        DefaultNewArchitectureEntryPoint.getConcurrentReactEnabled() // concurrentRootEnabled
     );
   }
 
   /**
    * Align the back button behavior with Android S
    * where moving root activities to background instead of finishing activities.
-   * @see <a href="https://developer.android.com/reference/android/app/Activity#onBackPressed()">onBackPressed</a>
+   * 
+   * @see <a href=
+   *      "https://developer.android.com/reference/android/app/Activity#onBackPressed()">onBackPressed</a>
    */
   @Override
   public void invokeDefaultOnBackPressed() {
--- src/components/tabbar/GameTrackerStack/GameTracker.tsx
+++ src/components/tabbar/GameTrackerStack/GameTracker.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { GameTrackerNavigationProps } from "../../../interfaces/INavigationProps";
+import Pressable from "../../general/Pressable";
 
 const GameTracker = (props: GameTrackerNavigationProps<"GameTracker">) => {
   return (
     <View>
       <Text>GameTracker</Text>
+      <Pressable onPress={() => props.navigation.navigate("TrackNewGame")}>
+        <Text>Track New Game</Text>
+      </Pressable>
     </View>
   );
 };
